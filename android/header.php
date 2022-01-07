a8d5c9ce55ac1699780f1d577fddd86d3eefb6af
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-01-07 16:28:06 +0530

    update

--- android/header.php
+++ android/header.php
@@ -22,7 +22,7 @@
 				                <a href="certificate.php"><i class="fas fa-coins"></i>&nbsp;&nbsp;&nbsp;Certificarte</a><hr>
 				                <!-- <a href="javascript:void(0)"><i class="fas fa-cog"></i>&nbsp;&nbsp;&nbsp;Settings</a><hr> -->
 								<a href="contact-us.php"><i class="far fa-building"></i>&nbsp;&nbsp;&nbsp;Contact Us</a><hr>
-			          			<a href="admission.php"><i class="fas fa-id-card-alt"></i>&nbsp;&nbsp;&nbsp;Addmission Now</a><hr>
+			          			<a href="admission.php"><i class="fas fa-id-card-alt"></i>&nbsp;&nbsp;&nbsp;Admission Now</a><hr>
 								<a href="result.php"><i class="fas fa-info-circle"></i>&nbsp;&nbsp;&nbsp;Results</a><hr>
 								<a href="pay.php"><i class="fa fa-credit-card" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;Pay Now</a><hr>
 								
